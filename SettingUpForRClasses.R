5b92cfe68d22e5c95aeb754bee8fa404739e6168
rtburg <ryan.thornburg@unc.edu>
2020-01-10 17:05:23 -0500

    Change link the setting up R for classes

---
+++ SettingUpForRClasses.R
@@ -0,0 +1,17 @@
+
+# part 1 ------------------------------------------------------------------
+
+install.packages("tidyverse")
+#readxl
+#lubridate
+#scales
+install.packages("janitor")
+
+
+# part 2 ------------------------------------------------------------------
+
+#Windows
+setwd('C:/Users/your-user-name-here/Documents/MEJO570')
+
+#Mac
+setwd('~/Documents/MEJO570')
